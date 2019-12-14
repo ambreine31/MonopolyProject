d7e310fc56bc853028874ad59645e852acdb2b1e
Ambre <bourdierambre@gmail.com>
2019-12-14 20:14:36 +0100

    adding chance cards as well as jail options

--- Player.cs
+++ Player.cs
@@ -12,15 +12,16 @@ namespace Monopoly
         private string token;
         private double money;
         private bool injail;
-        private List<Property> properties;
+        private int time;
 
         public Player(string token, double money)
         {
             this.token = token;
             this.money = money;
             this.injail = false;
-            this.properties = null;
             this.position = 0;
+            this.injail = false;
+            this.time = 0;
         }
 
         public int Position
@@ -46,28 +47,145 @@ namespace Monopoly
             set { this.injail = value; }
         }
 
-        public List<Property> Properties
+        public int RollDiceAndMove(Random generator)
         {
-            get { return this.properties; }
-            set { this.properties = value; }
-        }
-
-        public void RollDiceAndMove(Random generateur)
-        {
-            int dice1 = generateur.Next(1, 7);
-            int dice2 = generateur.Next(1, 7);
+            int dice1 = generator.Next(1, 7);
+            int dice2 = generator.Next(1, 7);
             Console.WriteLine(this.token + " rolled a " + dice1 + " and a " + dice2);
 
             int sum_dice = dice1 + dice2;
-            this.position = Loop(this.position + sum_dice);
+
+            if(this.injail == false)
+            {
+                this.position = Loop(this.position + sum_dice);
+                Console.WriteLine();
+                if(this.position == 30)
+                {
+                    GoToJail();
+                }
+                else if(this.position == 10)
+                {
+                    Console.WriteLine("Visiting the jail");
+                }
+                else if (this.position == 2 || this.position == 17 || this.position == 33)
+                {
+                    Console.WriteLine("Community Chest, you receive $100!");
+                    this.money = this.money + 100;
+                    Console.WriteLine("Your current balance is $" + this.money);
+                }
+                else if(this.position == 4)
+                {
+                    Console.WriteLine("Income tax, pay $200!");
+                    this.money = this.money - 200;
+                    Console.WriteLine("Your current balance is $" + this.money);
+                }
+                else if(this.position == 20)
+                {
+                    Console.WriteLine("Free Parking.");
+                }
+                else if(this.position == 38)
+                {
+                    Console.WriteLine("Luxury tax, pay $100");
+                    this.money = this.money - 200;
+                    Console.WriteLine("Your current balance is $" + this.money);
+                }
+                else if(this.position == 7 || this.position == 22 || this.position == 36)
+                {
+                    ChanceCards(generator);
+                }
+
+
+            }
+            if(dice1 == dice2)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
-        
-        static int Loop(int index)
+        public void ChanceCards(Random generator)
+        {
+            Console.WriteLine("Pick a Chance Card");
+            Console.ReadKey();
+            int choice  = generator.Next(1, 7);
+
+            if(choice == 1)
+            {
+                Console.WriteLine("Advance to START");
+                this.position = 0;
+                Console.WriteLine("You passed START, collect $200");
+                this.money = this.money + 200;
+                Console.WriteLine("Your current balance is $" + this.money);
+            }
+            if (choice == 2)
+            {
+                Console.WriteLine("Bank pays you a dividend of $50");
+                this.money = this.money + 50;
+                Console.WriteLine("Your current balance is $" + this.money);
+
+            }
+            if (choice == 3)
+            {
+                Console.WriteLine("Move forward 3 spaces");
+                this.position = this.position + 3;
+            }
+            if (choice == 4)
+            {
+                Console.WriteLine("Make reparations on your car, pay $20");
+                this.money = this.money - 20;
+                Console.WriteLine("Your current balance is $" + this.money);
+            }
+            if (choice == 5)
+            {
+                Console.WriteLine("Go to jail");
+                GoToJail();
+            }
+            if (choice == 6)
+            {
+                Console.WriteLine("You won the lottery! You earn $1000");
+                this.money = this.money + 1000;
+                Console.WriteLine("Your current balance is $" + this.money);
+            }
+            
+        }
+
+        public void GoToJail()
+        {
+            this.position = 10;
+            this.injail = true;
+            Console.WriteLine("You've been sent to jail!");
+            Console.WriteLine();
+        }
+
+        public void LeaveJail()
+        {
+            this.injail = false;
+            Console.WriteLine("You've been released from jail!");
+        }
+
+        public void TimeInJail()
+        {
+            time++;
+            if(time == 3)
+            {
+                Console.WriteLine("You've finished your jail sentence.");
+                LeaveJail();
+                time = 0;
+            }
+        }
+        public int Loop(int index)
         {
             if (index >= 40)
             {
                 index = index - 40;
+
+                Console.WriteLine("You passed START, collect $200");
+                this.money = this.money + 200;
+                Console.WriteLine("Your current balance is $" + this.money);
+                Console.ReadKey();
             }
             return index;
         }
@@ -78,43 +196,61 @@ namespace Monopoly
             {
                 if(p.Box_num == this.position)
                 {
+                    Console.WriteLine();
                     Console.WriteLine("This is property: " + p.Name);
                     if(p.Owner == null)
                     {
                         Console.WriteLine("This property is up for sale, price is $" + p.PropertyPrice);
                         Console.WriteLine("Your current balance is $" + this.money);
+                        Console.WriteLine();
                         Console.WriteLine("Do you wish to buy this property? (y or n)");
                         string response = Console.ReadLine();
                         if (response == "y")
                         {
                             this.money = this.money - p.PropertyPrice;
                             p.Owner = this;
-                            Console.WriteLine("You've aquired " + p.Name + " for $" + p.PropertyPrice);
+                            Console.WriteLine();
+                            Console.WriteLine("You've acquired " + p.Name + " for $" + p.PropertyPrice);
                             Console.WriteLine("Your current balance is $" + this.money);
                         }
+                        else
+                        {
+                            Console.WriteLine("You haven't acquired this property");
+                        }
                     }
-                    if(p.Owner != null)
+                    else if(p.Owner != null)
                     {
                         if(p.Owner == this)
                         {
+                            Console.WriteLine();
                             Console.WriteLine("This is your property, do you wish to buy a house or hotel?");
                             Console.WriteLine("Your current balance is $" + this.money);
                             Console.WriteLine("1. Buy a House for $" + p.PropertyPrice * 0.5);
                             Console.WriteLine("2. Buy a Hotel for $" + p.PropertyPrice);
                             Console.WriteLine("3. No purchase");
+                            Console.WriteLine();
                             Console.Write("Your choice: ");
+                            Console.WriteLine();
                             int choice = Convert.ToInt32(Console.ReadLine());
                             if(choice == 1)
                             {
                                 HouseDecorator house_decorator = new HouseDecorator(p);
                                 house_decorator.SetTotalPrice();
                                 Console.WriteLine("The total price of your property is now $" + p.TotalPrice.ToString());
+                                this.money = this.money - p.PropertyPrice * 0.5;
+                                Console.WriteLine("Your current balance is $" + this.money);
                             }
                             else if(choice == 2)
                             {
                                 HotelDecorator hotel_decorator = new HotelDecorator(p);
                                 hotel_decorator.SetTotalPrice();
                                 Console.WriteLine("The total price of your property is now $" + p.TotalPrice.ToString());
+                                this.money = this.money - p.PropertyPrice;
+                                Console.WriteLine("Your current balance is $" + this.money);
+                            }
+                            else
+                            {
+                                Console.WriteLine("No extra purchase was made");
                             }
                         }
                         if(p.Owner != this)
@@ -125,6 +261,7 @@ namespace Monopoly
                             Console.WriteLine("Your current balance is $" + this.money);
                             this.money = this.money - tax;
                             Console.WriteLine("After tax your current balance is $" + this.money);
+                            p.Owner.Money = p.Owner.Money + tax;
                         }                       
                     }
                 }
